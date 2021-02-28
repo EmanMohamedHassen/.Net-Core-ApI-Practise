453769ceaba413344a4c68e762a01e6d08f08a57
Eman Mohammed <emy_eva23@yahoo.com>
2021-02-28 13:14:36 +0200

    Validation with a Custom Attribute

--- CourseLibrary.API/Models/CourseForCreationDto.cs
+++ CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -1,15 +1,29 @@
-﻿using System;
+﻿using CourseLibrary.API.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CourseForCreationDto
+    [TitleDiffFromDescription]
+    public class CourseForCreationDto //:IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(1500)]
         public string Description { get; set; }
 
+        //public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        //{
+        //   if(Title == Description)
+        //    {
+        //        // yield return make sure the validation result are immediatily returned after which code execution will continue
+        //        yield return new ValidationResult("the provided description should be different from the title", new[] { "CourseForCreationDto" });// use class name as it class level validation 
+        //    }
+        //}
     }
 }

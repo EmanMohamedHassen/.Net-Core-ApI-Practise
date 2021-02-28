36aa391e7b322aa270644c86ce4e77dbf36a9f86
Eman Mohammed <emy_eva23@yahoo.com>
2021-02-28 16:00:41 +0200

    customize validation error responses

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -3,7 +3,9 @@ using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,9 +37,47 @@ namespace CourseLibrary.API
             services.AddControllers(setupAction =>
             {
                 setupAction.ReturnHttpNotAcceptable = true;
-               //setupAction.OutputFormatters.Add(
-               //    new XmlDataContractSerializerOutputFormatter());
-           }).AddXmlDataContractSerializerFormatters();
+                //setupAction.OutputFormatters.Add(
+                //    new XmlDataContractSerializerOutputFormatter());
+            }).AddXmlDataContractSerializerFormatters()
+           .ConfigureApiBehaviorOptions(setupAction =>
+           {
+               setupAction.InvalidModelStateResponseFactory = contexet =>
+               {
+                   // create a problem details object 
+                   var problemDetailsFactory = contexet.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+                   var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(contexet.HttpContext, contexet.ModelState);
+
+                   // add additional info added by default
+                   problemDetails.Detail = "see the errors fields for details.";
+                   problemDetails.Instance = contexet.HttpContext.Request.Path;
+
+                   //find out which status code to use 
+                   var actionExecutingContext = contexet as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                   //if there are modelstate errors & all arguments were correctly 
+                   // found / parsed we're dealing with validation errors 
+                   if ((contexet.ModelState.ErrorCount > 0) && (actionExecutingContext?.ActionArguments.Count == contexet.ActionDescriptor.Parameters.Count))
+                   {
+                       problemDetails.Type = "https://courseLibrary/modelvalidationproblem"; // custom type
+                       problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                       problemDetails.Title = "One or more validation errors occurred";
+                       return new UnprocessableEntityObjectResult(problemDetails)
+                       {
+                           ContentTypes = { "application/problem+json" } // from rfc standards 
+                        };
+                   }
+
+                   // if one of the arguments wasn't correctly found / couldn't be parsed
+                   // we're dealing with null/unparseable input 
+                   problemDetails.Status = StatusCodes.Status400BadRequest;
+                   problemDetails.Title = "One or more errors on input occurred.";
+                   return new BadRequestObjectResult(problemDetails)
+                   {
+                       ContentTypes = { "application/problem+json" }
+                   };
+               };
+           });
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
